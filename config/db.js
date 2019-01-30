caea4590d1ee6eca5835489ce27ba0e63fade6e0
batabana <bata@systemli.org>
2019-01-30 18:51:54 +0100

    clean up code, use async/await

--- config/db.js
+++ config/db.js
@@ -1,101 +1,72 @@
 var spicedPg = require("spiced-pg");
 var db = spicedPg(process.env.DATABASE_URL || "postgres:postgres:postgres@localhost:5432/socialnetwork");
 
-exports.createUser = (first, last, email, password) => {
-    return db
-        .query(
-            `INSERT INTO users (first, last, email, password)
-        VALUES ($1, $2, $3, $4)
-        RETURNING *`,
-            [first || null, last || null, email || null, password || null]
-        )
-        .then(results => {
-            return results.rows;
-        });
+exports.createUser = async (first, last, email, password) => {
+    const query = `
+    INSERT INTO users (first, last, email, password)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *
+    `;
+    const { rows } = await db.query(query, [first || null, last || null, email || null, password || null]);
+    return rows;
 };
 
-exports.getUserByEmail = email => {
-    return db
-        .query(
-            `SELECT id, password
-        FROM users
-        WHERE email = $1`,
-            [email]
-        )
-        .then(results => {
-            return results.rows;
-        });
+exports.getUserByEmail = async email => {
+    const query = `SELECT id, password FROM users WHERE email = $1`;
+    const { rows } = await db.query(query, [email]);
+    return rows;
 };
 
-exports.getUserById = id => {
-    return db
-        .query(
-            `SELECT id, first, last, email, image, bio
-        FROM users
-        WHERE id = $1`,
-            [id]
-        )
-        .then(results => {
-            return results.rows;
-        });
+exports.getUserById = async id => {
+    const query = `SELECT id, first, last, email, image, bio FROM users WHERE id = $1`;
+    const { rows } = await db.query(query, [id]);
+    return rows;
 };
 
-exports.saveImage = (url, id) => {
-    return db
-        .query(
-            `UPDATE users
-        SET image = $1
-        WHERE id = $2
-        RETURNING image`,
-            [url, id]
-        )
-        .then(results => {
-            return results.rows;
-        });
+exports.saveImage = async (url, id) => {
+    const query = `
+    UPDATE users
+    SET image = $1
+    WHERE id = $2
+    RETURNING image
+    `;
+    const { rows } = await db.query(query, [url, id]);
+    return rows;
 };
 
-exports.updateBio = (id, bio) => {
-    return db
-        .query(
-            `UPDATE users
-        SET bio = $2
-        WHERE id = $1
-        RETURNING *`,
-            [id, bio]
-        )
-        .then(results => {
-            return results.rows;
-        });
+exports.updateBio = async (id, bio) => {
+    const query = `
+    UPDATE users
+    SET bio = $2
+    WHERE id = $1
+    RETURNING *
+    `;
+    const { rows } = await db.query(query, [id, bio]);
+    return rows;
 };
 
-exports.getFriend = (id1, id2) => {
-    return db
-        .query(
-            `SELECT *
-        FROM friends
-        WHERE (receiver = $1 AND sender = $2)
-        OR (receiver = $2 AND sender = $1)`,
-            [id1, id2]
-        )
-        .then(results => {
-            return results.rows;
-        });
+exports.getFriend = async (id1, id2) => {
+    const query = `
+    SELECT *
+    FROM friends
+    WHERE (receiver = $1 AND sender = $2)
+    OR (receiver = $2 AND sender = $1)
+    `;
+    const { rows } = await db.query(query, [id1, id2]);
+    return rows;
 };
 
-exports.getFriends = id => {
-    return db
-        .query(
-            `SELECT users.id, first, last, image, accepted
-            FROM friends
-            JOIN users
-            ON (accepted = false AND receiver = $1 AND sender = users.id)
-            OR (accepted = true AND receiver = $1 AND sender = users.id)
-            OR (accepted = true AND sender = $1 AND receiver = users.id)`,
-            [id]
-        )
-        .then(results => {
-            return results.rows;
-        });
+exports.getFriends = async id => {
+    const query = `
+    SELECT users.id, first, last, image, accepted
+    FROM friends
+    JOIN users
+    ON (accepted = false AND receiver = $1 AND sender = users.id)
+    OR (accepted = true AND receiver = $1 AND sender = users.id)
+    OR (accepted = true AND sender = $1 AND receiver = users.id)
+    `;
+    const { rows } = await db.query(query, [id]);
+    return rows;
 };
 
 exports.makeFriend = (receiver, sender) => {
@@ -132,57 +103,53 @@ exports.deleteFriend = (id1, id2) => {
     );
 };
 
-exports.searchUsersByName = input => {
-    return db
-        .query(
-            `SELECT id, first, last, image
-        FROM users
-        WHERE LOWER(first) LIKE LOWER($1)
-        OR LOWER(last) LIKE LOWER($1)`,
-            [input + "%"]
-        )
-        .then(results => {
-            return results.rows;
-        });
+exports.searchUsersByName = async input => {
+    const query = `
+    SELECT id, first, last, image
+    FROM users
+    WHERE LOWER(first) LIKE LOWER($1)
+    OR LOWER(last) LIKE LOWER($1)
+    `;
+    const { rows } = await db.query(query, [input + "%"]);
+    return rows;
 };
 
-exports.getUsersByIds = arrayOfIds => {
+exports.getUsersByIds = async arrayOfIds => {
     const query = `SELECT id, first, last, image FROM users WHERE id = ANY($1)`;
-    return db.query(query, [arrayOfIds]).then(results => {
-        return results.rows;
-    });
+    const { rows } = await db.query(query, [arrayOfIds]);
+    return rows;
 };
 
-exports.getMessages = () => {
-    const query = `SELECT sender, first, last, image, message, messages.createtime AS createtime
+exports.getMessages = async () => {
+    const query = `
+    SELECT sender, first, last, image, message, messages.createtime AS createtime
     FROM messages
     JOIN users
     ON users.id = messages.sender
     ORDER BY messages.createtime DESC
-    LIMIT 10`;
-    return db.query(query).then(results => {
-        return results.rows;
-    });
+    LIMIT 10
+    `;
+    const { rows } = await db.query(query);
+    return rows;
 };
 
-exports.createMessage = (message, sender) => {
+exports.createMessage = async (message, sender) => {
     const query = `INSERT INTO messages(message,sender) VALUES ($1, $2) RETURNING id`;
-    return db.query(query, [message, sender]).then(results => {
-        return results.rows;
-    });
+    const { rows } = await db.query(query, [message, sender]);
+    return rows;
 };
 
-exports.getMessageById = id => {
+exports.getMessageById = async id => {
     const query = `
-        SELECT sender, first, last, image, message, messages.createtime AS createtime
-        FROM messages
-        JOIN users
-        ON users.id = messages.sender
-        WHERE messages.id = $1
-        ORDER BY messages.createtime DESC`;
-    return db.query(query, [id]).then(results => {
-        return results.rows;
-    });
+    SELECT sender, first, last, image, message, messages.createtime AS createtime
+    FROM messages
+    JOIN users
+    ON users.id = messages.sender
+    WHERE messages.id = $1
+    ORDER BY messages.createtime DESC
+    `;
+    const { rows } = await db.query(query, [id]);
+    return rows;
 };
 
 exports.getOpenFriendRequests = async id => {
--- src/actions.js
+++ src/actions.js
@@ -39,21 +39,21 @@ export async function searchUsers(input) {
     }
 }
 
-export async function listOnlineUsers(listOfOnlineUsers) {
+export function listOnlineUsers(listOfOnlineUsers) {
     return {
         type: "LIST_ONLINE_USERS",
         online_users: listOfOnlineUsers
     };
 }
 
-export async function addOnlineUser(newUser) {
+export function addOnlineUser(newUser) {
     return {
         type: "ADD_ONLINE_USER",
         new_user: [newUser]
     };
 }
 
-export async function deleteOnlineUser(oldUser) {
+export function deleteOnlineUser(oldUser) {
     return {
         type: "DELETE_ONLINE_USER",
         old_user: oldUser
